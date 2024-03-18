28d034abe7d7e83283f4ce622ea951704550790a
Masya <Masha-Sveta.1@yandex.ru>
2024-03-18 21:55:05 +0300

    крпимер2222ы

--- 2 SEMESTR/lab_3/lab_3.cpp
+++ 2 SEMESTR/lab_3/lab_3.cpp
@@ -12,26 +12,26 @@ int main(int argc, char *argv[])
     Text text;
     text.BIG_process("in3_1.txt", res);
 
-    // Text text1;
-    // text1.BIG_process("in2.txt", res);
+    Text text1;
+    text1.BIG_process("in3.txt", res);
 
-    // Text text2;
-    // text2.BIG_process("in2_2.txt", res);
+    Text text2;
+    text2.BIG_process("in3_2.txt", res);
 
-    // Text text3;
-    // text3.BIG_process("in2_3.txt", res);
+    Text text3;
+    text3.BIG_process("in3_3.txt", res);
 
-    // Text text4;
-    // text4.BIG_process("in2_4.txt", res);
+    Text text4;
+    text4.BIG_process("in3_4.txt", res);
 
-    // Text text5;
-    // text5.BIG_process("in2_5.txt", res);
+    Text text5;
+    text5.BIG_process("in3_5.txt", res);
 
-    // Text text6;
-    // text6.BIG_process("in2_6.txt", res);
+    Text text6;
+    text6.BIG_process("in3_6.txt", res);
 
-    // Text text7;
-    // text7.BIG_process("in2_7.txt", res);
+    Text text7;
+    text7.BIG_process("in3_7.txt", res);
 
     // Пройти по строкам и выделить предложения
     // Отсортировать предложения по количеству знаков препинания
