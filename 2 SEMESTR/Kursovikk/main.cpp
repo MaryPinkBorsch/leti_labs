273af4c229560899b959ce79465afddcd46057b4
Masya <Masha-Sveta.1@yandex.ru>
2024-04-06 19:45:25 +0300

    nachalo kursovik 2

---
+++ 2 SEMESTR/Kursovikk/main.cpp
@@ -0,0 +1,12 @@
+// нужны структуры:
+// 1) Еmployer, поля - name, work_field, adress, phone 
+// 2) Worker, поля - name, familia, otchestvo, wanted_profession, work_field, worked_time, salary
+// 3) Vacancia , поля - profession, grafik, salary, education_lvl  
+
+//4) birga_vacanciy - offers, resumes, closed_vacancis
+
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
