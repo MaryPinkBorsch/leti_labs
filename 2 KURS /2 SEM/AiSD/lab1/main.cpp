52ac84dfe2af8aead3c91ba79decce3b743b6e89
Masya <Masha-Sveta.1@yandex.ru>
2025-02-10 21:25:20 +0300

    Архиватор!

--- 2 KURS /2 SEM/AiSD/lab1/main.cpp
+++ 2 KURS /2 SEM/AiSD/lab1/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <stdio.h>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+
 #include "rle.h"
 
 using namespace std;
@@ -11,6 +15,7 @@ void print(const vector<unsigned char> &tmp)
     }
     cout << endl;
 }
+
 int main(int argc, char *argv[])
 {
     vector<unsigned char> tmp = {0xCF, 0xCF, 0xCF, 0xCF, 0xCF};
@@ -27,6 +32,38 @@ int main(int argc, char *argv[])
     
     cout << "разжатая строка: ";
     print(tmp2);
+
+    if (argc == 4) 
+    {
+        int mode = std::atoi(argv[1]); // 1 == compress, 2 == decompress
+        std::string input_filename = argv[2];
+        std::string output_filename = argv[3];
+        size_t input_file_size = std::filesystem::file_size(input_filename);
+
+        std::ifstream input_file( input_filename, std::ios::binary );
+
+        std::vector<unsigned char> input_data;
+        std::vector<unsigned char> output_data;
+        input_data.resize(input_file_size);
+
+        input_file.read((char*)input_data.data(), input_file_size);
+
+        switch(mode) 
+        {
+            case 1: // compress 
+            {
+                rle_compress_2_1(input_data, output_data);
+            }
+            break;
+            case 2: // compress 
+            {
+                rle_decompress_2_1(input_data, output_data);
+            }
+            break;
+        }
+        std::ofstream output_file( output_filename, std::ios::binary | std::ios::trunc);
+        output_file.write((char*)output_data.data(), output_data.size());
+    }
 
     return 0;
 }
