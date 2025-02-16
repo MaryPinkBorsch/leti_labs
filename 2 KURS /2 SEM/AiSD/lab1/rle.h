73544b00287417ecd459307e1c5598258dd0dff7
Masya <Masha-Sveta.1@yandex.ru>
2025-02-16 18:45:21 +0300

    задание 2.2 без проверки на баги pravka 2

--- 2 KURS /2 SEM/AiSD/lab1/rle.h
+++ 2 KURS /2 SEM/AiSD/lab1/rle.h
@@ -9,3 +9,9 @@ void rle_decompress_2_1(const std::vector<unsigned char> &input, std::vector<uns
 //второе задание 2.2
 void rle_compress_2_2(const std::vector<unsigned char> &input, std::vector<unsigned char> &output);
 void rle_decompress_2_2(const std::vector<unsigned char> &input, std::vector<unsigned char> &output);
+
+// 2.3
+//параметр М в битах (1 символ = 1 Байт = 8 ьит) это сколько символов РАЗНЫХ
+// может быть в повторяющейся последовательности
+
+// пример: abcabcabc ---> M =24 ---> 3 "abc"
