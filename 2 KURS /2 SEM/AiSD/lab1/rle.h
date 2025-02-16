1dbb21ab9170b3766394400e685171f3dc4fa7a4
Masya <Masha-Sveta.1@yandex.ru>
2025-02-16 18:46:52 +0300

    pravka 3

--- 2 KURS /2 SEM/AiSD/lab1/rle.h
+++ 2 KURS /2 SEM/AiSD/lab1/rle.h
@@ -11,7 +11,8 @@ void rle_compress_2_2(const std::vector<unsigned char> &input, std::vector<unsig
 void rle_decompress_2_2(const std::vector<unsigned char> &input, std::vector<unsigned char> &output);
 
 // 2.3
-//параметр М в битах (1 символ = 1 Байт = 8 ьит) это сколько символов РАЗНЫХ
+//параметр М в битах (1 символ = 1 Байт = 8 ьит) это сколько чарсимволов РАЗНЫХ
 // может быть в повторяющейся последовательности
 
-// пример: abcabcabc ---> M =24 ---> 3 "abc"
+// пример: abcabcabc ---> M =24 бит---> 3 "abc" (повторябщийся символ это "abc" занимает 3 байта или 24 бита)
+// М надо найти самим в процесее
