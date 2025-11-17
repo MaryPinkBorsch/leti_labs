4b53acfb1f045d48d41f47a5ee99831431efdd00
Masya <Masha-Sveta.1@yandex.ru>
2025-11-17 21:19:27 +0300

    service impl

---
+++ 3 KURS/Kotlin/Praktika/src/main/java/com/example/masya/sportstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.masya.sportstore.service.impl;
+
+import com.example.masya.sportstore.entity.User;
+import com.example.masya.sportstore.repository.UserRepository;
+import com.example.masya.sportstore.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
